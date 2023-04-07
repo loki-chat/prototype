8b86d631609f73efd84d5ed5894b1a5fb959fb08
Tobias SN <15670490+TobiasSN@users.noreply.github.com>
2023-04-07 15:26:04 +0200

    chore(frontend-prototype): basic window

--- loki-frontend-prototype/src/main.rs
+++ loki-frontend-prototype/src/main.rs
@@ -1,3 +1,27 @@
+use eframe::egui;
+
 fn main() {
-	println!("Hello, world!");
+	let native_options = eframe::NativeOptions::default();
+	eframe::run_native(
+		"Loki (Prototype)",
+		native_options,
+		Box::new(|cc| Box::new(App::new(cc))),
+	)
+	.expect("Failed to create window");
+}
+
+struct App;
+
+impl App {
+	fn new(_cc: &eframe::CreationContext<'_>) -> Self {
+		Self {}
+	}
+}
+
+impl eframe::App for App {
+	fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+		egui::CentralPanel::default().show(ctx, |ui| {
+			ui.heading("Hello, World!");
+		});
+	}
 }
