6ebb16a713a84bc33e6d7e5e1144c3661f9d7377
Speykious <speykious@gmail.com>
2023-04-17 13:21:43 +0200

    Simplify `Anchor`

--- lokui/src/layout/anchor.rs
+++ lokui/src/layout/anchor.rs
@@ -1,86 +1,25 @@
 use super::SolvedLayout;
 
-#[derive(Clone, Copy, Debug, PartialEq, Eq, PartialOrd, Ord, Default)]
-pub enum HAnchor {
-	Left,
-	#[default]
-	Center,
-	Right,
-}
-
-impl HAnchor {
-	pub fn calc_x_offset(&self, inner_width: f32, outer_width: f32) -> f32 {
-		match self {
-			HAnchor::Left => 0.,
-			HAnchor::Center => (outer_width - inner_width) / 2.,
-			HAnchor::Right => outer_width - inner_width,
-		}
-	}
-}
-
-#[derive(Clone, Copy, Debug, PartialEq, Eq, PartialOrd, Ord, Default)]
-pub enum VAnchor {
-	Top,
-	#[default]
-	Center,
-	Bottom,
-}
-
-impl VAnchor {
-	pub fn calc_y_offset(&self, inner_height: f32, outer_height: f32) -> f32 {
-		match self {
-			VAnchor::Top => 0.,
-			VAnchor::Center => (outer_height - inner_height) / 2.,
-			VAnchor::Bottom => outer_height - inner_height,
-		}
-	}
-}
-
-#[derive(Clone, Copy, Debug, PartialEq, Eq, Default)]
+#[derive(Clone, Copy, Debug, Default)]
 pub struct Anchor {
-	pub v: VAnchor,
-	pub h: HAnchor,
+	pub x: f32,
+	pub y: f32,
 }
 
 impl Anchor {
-	pub const TOP_LEFT: Anchor = Anchor {
-		v: VAnchor::Top,
-		h: HAnchor::Left,
-	};
-	pub const TOP_CENTER: Anchor = Anchor {
-		v: VAnchor::Top,
-		h: HAnchor::Center,
-	};
-	pub const TOP_RIGHT: Anchor = Anchor {
-		v: VAnchor::Top,
-		h: HAnchor::Right,
-	};
+	pub const fn new(x: f32, y: f32) -> Self {
+		Self { x, y }
+	}
 
-	pub const CENTER_LEFT: Anchor = Anchor {
-		v: VAnchor::Center,
-		h: HAnchor::Left,
-	};
-	pub const CENTER: Anchor = Anchor {
-		v: VAnchor::Center,
-		h: HAnchor::Center,
-	};
-	pub const CENTER_RIGHT: Anchor = Anchor {
-		v: VAnchor::Center,
-		h: HAnchor::Right,
-	};
-
-	pub const BOTTOM_LEFT: Anchor = Anchor {
-		v: VAnchor::Bottom,
-		h: HAnchor::Left,
-	};
-	pub const BOTTOM_CENTER: Anchor = Anchor {
-		v: VAnchor::Bottom,
-		h: HAnchor::Center,
-	};
-	pub const BOTTOM_RIGHT: Anchor = Anchor {
-		v: VAnchor::Bottom,
-		h: HAnchor::Right,
-	};
+	pub const TOP_LEFT: Anchor = Anchor::new(0.0, 0.0);
+	pub const TOP_CENTER: Anchor = Anchor::new(0.5, 0.0);
+	pub const TOP_RIGHT: Anchor = Anchor::new(1.0, 0.0);
+	pub const CENTER_LEFT: Anchor = Anchor::new(0.0, 0.5);
+	pub const CENTER: Anchor = Anchor::new(0.5, 0.5);
+	pub const CENTER_RIGHT: Anchor = Anchor::new(1.0, 0.5);
+	pub const BOTTOM_LEFT: Anchor = Anchor::new(0.0, 1.0);
+	pub const BOTTOM_CENTER: Anchor = Anchor::new(0.5, 1.0);
+	pub const BOTTOM_RIGHT: Anchor = Anchor::new(1.0, 1.0);
 
 	pub fn calc_child_abs_pos(
 		&self,
@@ -88,12 +27,12 @@ impl Anchor {
 		height: f32,
 		parent_layout: &SolvedLayout,
 	) -> SolvedLayout {
-		let x = self.h.calc_x_offset(width, parent_layout.width());
-		let y = self.v.calc_y_offset(height, parent_layout.height());
+		let x_offset = (parent_layout.width() - width) * self.x;
+		let y_offset = (parent_layout.height() - height) * self.y;
 
 		SolvedLayout {
-			x: parent_layout.x + x,
-			y: parent_layout.y + y,
+			x: parent_layout.x + x_offset,
+			y: parent_layout.y + y_offset,
 			width,
 			height,
 		}
