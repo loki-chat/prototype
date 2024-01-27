72cdf7d79e498b62acf3973a62b87af40632fa59
somehybrid <somehybrid@not-a-real.email>
2024-01-27 19:46:41 +0700

    Encrypted messages

--- protolok/src/lib.rs
+++ protolok/src/lib.rs
@@ -1,12 +1,9 @@
 pub mod channel;
-pub mod crypto;
 pub mod ids;
-pub mod message;
 pub mod place;
 pub mod user;
 
 pub use channel::*;
 pub use ids::*;
-pub use message::*;
 pub use place::*;
 pub use user::*;
