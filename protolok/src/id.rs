d06ad473ad3df2cac25454c004aa13cde76e772d
somehybrid <somehybrid@not-a-real.email>
2024-02-01 07:43:27 +0700

    Snowflake refactoring

--- protolok/src/id.rs
+++ protolok/src/id.rs
@@ -1,4 +1,7 @@
-use std::time::{Duration, SystemTime, UNIX_EPOCH};
+use std::{
+	sync::Mutex,
+	time::{Duration, SystemTime, UNIX_EPOCH},
+};
 
 const EPOCH: Duration = Duration::from_millis(1672531200000);
 
@@ -6,38 +9,36 @@ const fn mask(amount: u64) -> u64 {
 	(1 << amount) - 1
 }
 
-pub struct Snowflake {
+pub struct SnowflakeGenerator {
 	last_evoked: u64,
-	count: u16,
-	worker_id: u16,
+	count: Mutex<u16>,
 }
 
-impl Snowflake {
-	pub fn new(worker_id: u16) -> Snowflake {
-		Snowflake {
+impl SnowflakeGenerator {
+	pub fn new() -> SnowflakeGenerator {
+		SnowflakeGenerator {
 			last_evoked: 0,
-			count: 0,
-			worker_id,
+			count: Mutex::new(0),
 		}
 	}
 
-	pub fn generate(&mut self) -> u64 {
+	pub fn generate(&mut self, worker_id: u16) -> u64 {
 		let timestamp = SystemTime::now()
 			.duration_since(UNIX_EPOCH + EPOCH)
 			.expect("Time went backwards")
 			.as_millis() as u64
 			& mask(42);
 
+		let mut count = self.count.lock().expect("Poisoned mutex");
+
 		if self.last_evoked > timestamp {
-			self.count = 0;
+			*count = 0;
 			self.last_evoked = timestamp;
 		}
 
-		let id = timestamp << 22
-			| (self.worker_id as u64 & mask(10)) << 12
-			| (self.count as u64 & mask(12));
+		let id = timestamp << 22 | (worker_id as u64 & mask(10)) << 12 | (*count as u64 & mask(12));
 
-		self.count += 1;
+		*count += 1;
 
 		id
 	}
@@ -55,12 +56,19 @@ pub trait Object {
 
 #[cfg(test)]
 mod tests {
-	use super::Snowflake;
+	use super::SnowflakeGenerator;
 
 	#[test]
 	fn test_snowflake_count() {
-		let mut snowflake = Snowflake::new(0);
+		let mut snowflake = SnowflakeGenerator::new();
 
-		assert_ne!(snowflake.generate(), snowflake.generate());
+		assert_ne!(snowflake.generate(0), snowflake.generate(0));
 	}
+
+    #[test]
+    fn test_snowflake_worker() {
+        let mut snowflake = SnowflakeGenerator::new();
+
+        assert_ne!(snowflake.generate(1), snowflake.generate(0)); 
+    }
 }
